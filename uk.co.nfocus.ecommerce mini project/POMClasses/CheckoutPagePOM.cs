1aab6cb767f70ad8d27bab1182b18d7c6fa16625
James Churcher <james.churcher@nfocus.co.uk>
2024-02-17 22:11:20 +0000

    Started test case 2, Added checkout page POM

---
+++ uk.co.nfocus.ecommerce mini project/POMClasses/CheckoutPagePOM.cs
@@ -0,0 +1,131 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static uk.co.nfocus.ecommerce_mini_project.Utilities.TestHelper;
+
+namespace uk.co.nfocus.ecommerce_mini_project.POMClasses
+{
+    internal class CheckoutPagePOM
+    {
+        private IWebDriver _driver;
+
+        public CheckoutPagePOM(IWebDriver driver)
+        {
+            this._driver = driver;  //Provide driver
+
+            Assert.That(_driver.Url,
+                        Does.Contain("checkout"),
+                        "Not on the checkout page");   //Verify we are on the correct page
+        }
+
+        //Locators
+
+        private IWebElement _firstNameField => _driver.FindElement(By.Id("billing_first_name"));
+        //public string SetFirstName
+        //{
+        //    get => _firstNameField.Text;
+        //    set => _firstNameField.SendKeys(value);
+        //}
+
+        private IWebElement _lastNameField => _driver.FindElement(By.Id("billing_last_name"));
+        private IWebElement _countryDropDown => _driver.FindElement(By.Id("billing_country"));
+        private IWebElement _streetField => _driver.FindElement(By.Id("billing_address_1"));
+        private IWebElement _cityField => _driver.FindElement(By.Id("billing_city"));
+        private IWebElement _postcodeField => _driver.FindElement(By.Id("billing_postcode"));
+        private IWebElement _phoneNumberField => _driver.FindElement(By.Id("billing_phone"));
+        private IWebElement _placeOrderButton => _driver.FindElement(By.Id("place_order"));
+        //private IWebElement _placeOrderButton => _driver.FindElement(By.LinkText("Place order"));
+
+        //Service methods
+
+        // Set the street in the first street field
+        public CheckoutPagePOM SetFirstName(string name)
+        {
+            _firstNameField.Clear();
+            _firstNameField.SendKeys(name);
+            return this;
+        }
+
+        // Set the street in the last street field
+        public CheckoutPagePOM SetLastName(string name)
+        {
+            _lastNameField.Clear();
+            _lastNameField.SendKeys(name);
+            return this;
+        }
+
+        // Set the street in the street address field
+        public CheckoutPagePOM SetStreetAddress(string street)
+        {
+            _streetField.Clear();
+            _streetField.SendKeys(street);
+            return this;
+        }
+
+        // Set the town or city in the city field
+        public CheckoutPagePOM SetCityField(string city)
+        {
+            _cityField.Clear();
+            _cityField.SendKeys(city);
+            return this;
+        }
+
+        // Set the postcode in the postcode field
+        public CheckoutPagePOM SetPostcodeField(string postcode)
+        {
+            _postcodeField.Clear();
+            _postcodeField.SendKeys(postcode);
+            return this;
+        }
+
+        // Set the phone number in the phone number field
+        public CheckoutPagePOM SetPhoneNumberField(string phoneNumber)
+        {
+            _phoneNumberField.Clear();
+            _phoneNumberField.SendKeys(phoneNumber);
+            return this;
+        }
+
+        // Select country from the dropdown
+        public CheckoutPagePOM SelectCounrtyDropdown(string country)
+        {
+            new SelectElement(_countryDropDown).SelectByText(country);
+            return this;
+        }
+
+        // Place and submit the order by clicking the place order button
+        public void ClickPlaceOrder()
+        {
+            Console.WriteLine("Position: " + _driver.FindElement(By.Id("payment")).GetCssValue("position"));
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(4)).Until(drv => drv.FindElement(By.Id("payment")).GetCssValue("position")=="relative");
+            //WaitForElDisplayed(_driver, By.Id("place_order"));
+
+            Console.WriteLine("Before click");
+            //Thread.Sleep(15000);
+            _placeOrderButton.Click();
+            Console.WriteLine("After click");
+        }
+
+        //Highlevel service methods
+
+        public void CheckoutExpectSuccess(string firstName, string lastName, string country, string street, string city, string postcode, string phoneNumber)
+        {
+            SetFirstName(firstName);
+            SetLastName(lastName);
+            SetStreetAddress(street);
+            SetCityField(city);
+            SetPostcodeField(postcode);
+            SetPhoneNumberField(phoneNumber);
+
+            SelectCounrtyDropdown(country);
+
+            //Thread.Sleep(4000);
+
+            ClickPlaceOrder();
+        }
+    }
+}
--- uk.co.nfocus.ecommerce mini project/UnitTest1.cs
+++ uk.co.nfocus.ecommerce mini project/UnitTest1.cs
@@ -11,8 +11,12 @@ namespace uk.co.nfocus.ecommerce_mini_project
     internal class Tests : BaseTest
     {
         private const double couponWorth = 0.15;
-        private const int shippingCost = 395;
+        private const int shippingCost = 395;       //TO:DO > Get shipping cost from website dynamically
 
+        /* First test case
+         * Logs in and attempts to apply discount to a cart with items in, then logs out.
+         * Tests login/logout, discount and shipping calculation.
+         */
         [TestCase("newexampleemail@email.com", "MyPassword12345@", "edgewords")]
         public void TestCase1(string testUsername, string testPassword, string testDiscountCode)
         {
@@ -70,32 +74,102 @@ namespace uk.co.nfocus.ecommerce_mini_project
             // Assess coupon removes 15%
             try     //Verify coupon amount
             {
-                Assert.That(actualDiscount, Is.EqualTo(expectedDiscount)); 
-                Console.WriteLine($"15% discount amount ->\n\tExpected: {actualDiscount}, Actual: {actualDiscount}");
+                Assert.That(actualDiscount, Is.EqualTo(expectedDiscount));
             }
             catch (Exception)
             {
                 //Do nothing
             }
+            Console.WriteLine($"15% discount amount ->\n\tExpected: {actualDiscount}, Actual: {actualDiscount}");
 
             // Get and Calculate actual and expected totals
             int expectedTotal = (int)(price * (1 - couponWorth)) + shippingCost;
             int actualTotal = StringToInt(cartPage.GetCartTotal());
+            //Thread.Sleep(4000);
+            //Console.WriteLine("The actual total is " + actualDiscount);
 
             // Assess final total is correct
             try     //Verify final price
             {
                 Assert.That(actualTotal, Is.EqualTo(expectedTotal));
-                Console.WriteLine($"Final price ->\n\tExpected: {expectedTotal}, Actual: {actualTotal}");
             }
             catch (Exception)
             {
                 //Do nothing
             }
+            Console.WriteLine($"Final price ->\n\tExpected: {expectedTotal}, Actual: {actualTotal}");
+
+            // Test Teardown ----------------------
 
             // Remove discount and items from cart
             cartPage.MakeCartEmpty();   //Remove the discount and products
             Console.WriteLine("Remove items from cart");
+
+            // Logout
+            navBar.GoAccount();
+            TestHelper.WaitForElDisplayed(driver, By.LinkText("Logout"));   //Wait for account page to load
+
+            //Attempt logout
+            bool logoutStatus = loginPage.LogoutExpectSuccess();
+            Assert.That(logoutStatus, "Could not logout");   //Verify successful login
+
+            Console.WriteLine("Logout from account");
+
+            Console.WriteLine("--Test Complete!--");
+        }
+
+        /* Second test case
+         * Logs in and attempts to checkout an item, then logs out.
+         * Tests login/logout, billing information form, checkout functionality, order creation.
+         */
+        [TestCase("newexampleemail@email.com", "MyPassword12345@", "edgewords")]
+        public void TestCase2(string testUsername, string testPassword, string testDiscountCode)
+        {
+            // Go to shop url
+            driver.Navigate().GoToUrl("https://www.edgewordstraining.co.uk/demo-site/");
+            Console.WriteLine("Navigated to site");
+
+            // Dismiss popup
+            driver.FindElement(By.LinkText("Dismiss")).Click();
+
+            // Create NavBar POM instance
+            NavBarPOM navBar = new(driver);
+
+            // Navigate to account login page
+            navBar.GoAccount();
+            Console.WriteLine("Navigated to login page");
+
+            // Login to said account
+            LoginPagePOM loginPage = new(driver);
+
+            //Provide username, password, and click
+            bool loginStatus = loginPage.LoginExpectSuccess(testUsername, testPassword);
+            Assert.That(loginStatus, "Could not login");   //Verify successful login
+            Console.WriteLine("Login complete");
+
+            // Enter the shop
+            navBar.GoShop();
+            Console.WriteLine("Navigated to shop");
+
+            // Add to basket
+            ShopPagePOM shopPage = new(driver);
+            shopPage.ClickAddToBasket();
+            Console.WriteLine("Add item to cart");
+
+            // Go to checkout
+            navBar.GoCheckout();
+            Console.WriteLine("Navigated to checkout");
+
+            // Enter billing information
+            CheckoutPagePOM checkoutPage = new(driver);
+            //checkoutPage.SetFirstName = "George";
+            //checkoutPage.SetFirstName("Joseph");
+            Console.WriteLine("Enter billing information");
+            //checkoutPage.CheckoutExpectSuccess("Jeff", "Bezos", "Poland", "Amazon lane", "New York", "20500", "00000000000");
+            Console.WriteLine("Checkout");
+
+
+            // Test Teardown ----------------------
 
             // Logout
             navBar.GoAccount();
